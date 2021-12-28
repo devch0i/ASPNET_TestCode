9ea213df56e15170ddbfb51bd2c32edc08ce6678
3536cjw@gmail.com <3536cjw@gmail.com>
2021-12-28 17:55:54 +0900

    미완성

---
+++ ASPNET_TestCode/211228/CoffeeRecipe.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNET_TestCode._211228
+{
+    public partial class CoffeeRecipe : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) { 
+                CoffeeType
+            }
+        }
+    }
+}
