c468249d9aadc629eb60f52aeafdc5a69cdafe4c
3536cjw@gmail.com <3536cjw@gmail.com>
2021-12-29 14:49:42 +0900

    커피 레시피 출력

--- ASPNET_TestCode/211228/CoffeeRecipe.aspx.cs
+++ ASPNET_TestCode/211228/CoffeeRecipe.aspx.cs
@@ -11,9 +11,36 @@ namespace ASPNET_TestCode._211228
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (!IsPostBack) { 
-                CoffeeType
+            if (!IsPostBack) {
+                CoffeeType.Items.Clear();
+                CoffeeType.Items.Add(new ListItem("밀크 커피(설탕+프림)", "0"));
+                CoffeeType.Items.Add(new ListItem("프림 커피(프림)", "1"));
+                CoffeeType.Items.Add(new ListItem("설탕 커피(설탕)", "2"));
+                CoffeeType.Items.Add(new ListItem("블랙 커피", "3"));
             }
+        }
+        protected void Show_ServerClick(object sender, EventArgs e) {
+            // 파일을 불러올 물리 경로 얻기
+            string filePath = Request.PhysicalApplicationPath + @"App_Data\";
+            string fileName = "";
+
+            Response.Write("<ol>");
+
+            // 선택한 항목으로 커피 종류 할당
+            ListItem item = CoffeeType.Items[CoffeeType.SelectedIndex];
+            int coffeeType = int.Parse(item.Value);
+
+            for (int i = 0; i < 4; i++) {
+                fileName = filePath + i + ".txt";
+                Response.WriteFile(fileName);
+
+                if (i != 3 && ((i & coffeeType) == 1 || (i & coffeeType) == 2))
+                    Response.Clear();
+
+                Response.Flush();
+            }
+
+            Response.Write("</ol>");
         }
     }
 }
