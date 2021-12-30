effc9d98fd03fdecd6c31e675fbea62354a8ff06
3536cjw@gmail.com <3536cjw@gmail.com>
2021-12-30 14:21:05 +0900

    버튼 컨트롤 실습

---
+++ ASPNET_TestCode/211230/ButtonExam.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ButtonExam.aspx.cs" Inherits="ASPNET_TestCode._211230.ButtonExam" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+    <script type="text/javascript">
+        function ConfirmGoHanbit() { return confirm("한빛미디어로 이동하시겠습니까?"); }
+    </script>
+    <style type="text/css">
+        .auto-style1 {
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:Button ID="btnSortAscending" runat="server" Text="오름차순 정렬" CommandName="Sort" CommandArgument="Ascending" OnCommand="CommandBtn_Click" />
+            <asp:Button ID="btmSortDescending" runat="server" Text="내림차순 정렬" CommandName="Sort" CommandArgument="Descending" OnCommand="CommandBtn_Click" /><br /><br />
+            <asp:LinkButton ID="lbtnGoHanbit" runat="server" OnClientClick="return ConfirmGoHanbit()" OnClick="lbtnGoHanbit_Click">Go! Hanbit Media, Inc.</asp:LinkButton><br /><br />
+            <asp:ImageButton ID="ibtnHanbit" runat="server" ImageUrl="~/images/hanbit.png" OnClick="ibtnHanbit_Click" /><br /><br />
+            <asp:Label ID="lblMessage" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPNET_TestCode/211230/ButtonExam.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNET_TestCode._211230
+{
+    public partial class ButtonExam : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblMessage.Text = "";
+        }
+
+        protected void CommandBtn_Click(object sender, CommandEventArgs e)
+        {
+            switch (e.CommandName) {
+                case "Sort":
+                    Sort_List((String)e.CommandArgument);
+                    break;
+                default:
+                    lblMessage.Text = "CommandName을 확인해주세요.";
+                    break;
+            }
+        }
+
+        void Sort_List(string commandArgument) {
+            switch (commandArgument) {
+                case "Ascending":
+                    lblMessage.Text = "자료를 오름차순으로 정렬하였습니다.";
+                    break;
+                case "Descending":
+                    lblMessage.Text = "자료를 내림차순으로 정렬하였습니다.";
+                    break;
+                default:
+                    lblMessage.Text = "CommandArgument를 확인해주세요.";
+                    break;
+            }
+        }
+
+        protected void lbtnGoHanbit_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://www.hanb.co.kr/");
+        }
+
+        protected void ibtnHanbit_Click(object sender, ImageClickEventArgs e)
+        {
+            lblMessage.Text = "그림 위 (" + e.X.ToString() + ", ";
+            lblMessage.Text += e.Y.ToString() + ") (을)를 클릭하였습니다.";
+        }
+    }
+}
