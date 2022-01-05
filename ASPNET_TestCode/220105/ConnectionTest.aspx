89f7a7f4673c6b42a895c5996d1c1606e67e1698
3536cjw@gmail.com <3536cjw@gmail.com>
2022-01-05 17:05:48 +0900

    db 연결 아직 못함

---
+++ ASPNET_TestCode/220105/ConnectionTest.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ConnectionTest.aspx.cs" Inherits="ASPNET_TestCode._220105.ConnectionTest" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            데이터베이스 연결 실습<br />
+            <br />
+            <asp:Button ID="btnConnection" runat="server" Text="연결 열기" OnClick="btnConnection_Click" />
+            <br />
+            <br />
+            <asp:Label ID="lblConnectionInformation" runat="server" Text=""></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASPNET_TestCode/220105/ConnectionTest.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ASPNET_TestCode._220105
+{
+    public partial class ConnectionTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConnection_Click(object sender, EventArgs e)
+        {
+            s
+            string connectionString = WebConfigurationManager.ConnectionStrings["AdventureWorks"].ConnectionString;
+
+            SqlConnection conn = new SqlConnection(connectionString);
+
+            lblConnectionInformation.Text = "<font color='red'>";
+
+            try
+            {
+                conn.Open();
+                lblConnectionInformation.Text += "<b>서버 버전 : </b>" + conn.ServerVersion;
+                lblConnectionInformation.Text += "<br/><br/>연결 상태 : </b>" + conn.State.ToString();
+            }
+            catch(Exception error)
+            {
+                lblConnectionInformation.Text += "<b>데이터베이스 읽기 오류</b><br/>";
+                lblConnectionInformation.Text += error.Message;
+            }
+            finally { 
+                conn.Close();
+
+                lblConnectionInformation.Text += "<br/><b>마지막 연결 상태 : </b>" + conn.State.ToString();
+            }
+            lblConnectionInformation.Text += "</font>";
+            
+        }
+    }
+}
